8f18733d3f6dfc10bbf0f9bc0ec97c8953c9294e
Sheng Di <SDi@walmartlabs.com>
2017-08-04 11:02:25 -0700

    few changes in task menu

--- ignite.js
+++ ignite.js
@@ -1,10 +1,5 @@
 "use strict";
 
-const logger = require("./lib/logger");
-const chalk = require("chalk");
-const pkg = require("./package.json");
-const xsh = require("xsh");
-const Path = require("path");
 const readline = require("readline");
 const taskLoader = require("./lib/task-loader");
 
@@ -14,69 +9,45 @@ const rl = readline.createInterface({
 });
 
 function igniteCore(type, task) {
-  logger.log(chalk.green(`Welcome to electrode-ignite version ${pkg.version}`));
-  logger.log(chalk.green("Checking latest version available on npm"));
+  if (!task) {
+    console.log(
+      "---------------------------------------------------------\n" +
+      " * * * * * * * Electrode Ignite Menu * * * * * * * * * * \n" +
+      "---------------------------------------------------------\n" +
+      "[1] Install tools for Electrode development\n" +
+      "[2] Check your NodeJS and npm environment\n" +
+      "[3] Check Electrode generator and tools installed\n" +
+      "[4] Generate an Electrode application\n" +
+      "[5] Generate an Electrode component\n" +
+      "[6] Add a component to your existing component repo\n" +
+      "---------------------------------------------------------\n"
+    );
 
-  xsh
-    .exec(true, "npm show electrode-ignite version")
-    .then(function(latestVersion) {
-      var version = latestVersion.stdout.slice(0, -1);
-
-      if (version !== pkg.version) {
-        logger.log(
-          chalk.yellow(
-            `Latest version is ${version} - trying to update.\nelectrode-ignite updated to ${version}, exiting, please run your command again.`
-          )
-        );
-      } else {
-        logger.log(
-          chalk.green("You've aleady installed the latest electrode-ignite.\n")
-        );
-      }
-
-      if (!task) {
-        console.log(
-          "---------------------------------------------------------\n"
-        );
-        console.log(
-          " * * * * * * * Electrode Ignite Menu * * * * * * * * * * \n"
-        );
-        console.log(
-          "---------------------------------------------------------\n"
-        );
-        console.log("[1] Install tools for Electrode development\n");
-        console.log("[2] Check your NodeJS and npm environment\n");
-        console.log("[3] Check Electrode generator and tools installed\n");
-        console.log("[4] Generate an Electrode application\n");
-        console.log("[5] Generate an Electrode component\n");
-        console.log("[6] Add a component to your existing component repo\n");
-
-        rl.question("Please select your option ", answer => {
-          taskLoader(answer, type);
-          rl.close();
-        });
-      } else if (task === "install") {
-        taskLoader("1");
-        rl.close();
-      } else if (task === "check-nodejs") {
-        taskLoader("2");
-        rl.close();
-      } else if (task === "check-electrode") {
-        taskLoader("3", type);
-        rl.close();
-      } else if (task === "generate-app") {
-        taskLoader("4", type);
-        rl.close();
-      } else if (task === "generate-component") {
-        taskLoader("5", type);
-        rl.close();
-      } else if (task === "add-component") {
-        taskLoader("6", type);
-        rl.close();
-      } else {
-        console.log("Please provide a valid task");
-      }
+    rl.question("Please select your option\n", answer => {
+      taskLoader(answer, type);
+      rl.close();
     });
+  } else if (task === "install") {
+    taskLoader("1");
+    rl.close();
+  } else if (task === "check-nodejs") {
+    taskLoader("2");
+    rl.close();
+  } else if (task === "check-electrode") {
+    taskLoader("3", type);
+    rl.close();
+  } else if (task === "generate-app") {
+    taskLoader("4", type);
+    rl.close();
+  } else if (task === "generate-component") {
+    taskLoader("5", type);
+    rl.close();
+  } else if (task === "add-component") {
+    taskLoader("6", type);
+    rl.close();
+  } else {
+    console.log("Please provide a valid task");
+  }
 }
 
 module.exports = igniteCore;
