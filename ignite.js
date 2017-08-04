71d5ee49e1a1011d6d4b5c8c8f698fe2295e5136
Sheng Di <SDi@walmartlabs.com>
2017-08-04 13:06:02 -0700

    ignite install

--- ignite.js
+++ ignite.js
@@ -2,34 +2,35 @@
 
 const readline = require("readline");
 const taskLoader = require("./lib/task-loader");
+const errorHandler = require("./lib/error-handler");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
 });
 
 function igniteCore(type, task) {
   if (!task) {
     console.log(
       "---------------------------------------------------------\n" +
-      " * * * * * * * Electrode Ignite Menu * * * * * * * * * * \n" +
-      "---------------------------------------------------------\n" +
-      "[1] Install tools for Electrode development\n" +
-      "[2] Check your NodeJS and npm environment\n" +
-      "[3] Check Electrode generator and tools installed\n" +
-      "[4] Generate an Electrode application\n" +
-      "[5] Generate an Electrode component\n" +
-      "[6] Add a component to your existing component repo\n" +
-      "---------------------------------------------------------\n"
+        " * * * * * * * Electrode Ignite Menu * * * * * * * * * * \n" +
+        "---------------------------------------------------------\n" +
+        "[1] Install tools for Electrode development\n" +
+        "[2] Check your NodeJS and npm environment\n" +
+        "[3] Check Electrode generator and tools installed\n" +
+        "[4] Generate an Electrode application\n" +
+        "[5] Generate an Electrode component\n" +
+        "[6] Add a component to your existing component repo\n" +
+        "---------------------------------------------------------\n"
     );
 
-    rl.question("Please select your option\n", answer => {
+    rl.question("Please select your option: ", answer => {
       taskLoader(answer, type);
       rl.close();
     });
   } else if (task === "install") {
     taskLoader("1");
-    rl.close();
   } else if (task === "check-nodejs") {
     taskLoader("2");
     rl.close();
@@ -46,7 +47,19 @@ function igniteCore(type, task) {
     taskLoader("6", type);
     rl.close();
   } else {
-    console.log("Please provide a valid task");
+    errorHandler(
+      "Please provide a valid task name. You can choose from: \n" +
+      "---------------------------------------------------------\n" +
+        " * * * * * * * Electrode Ignite Menu * * * * * * * * * * \n" +
+        "---------------------------------------------------------\n" +
+        "- install\n" +
+        "- check-nodejs\n" +
+        "- check-electrode\n" +
+        "- generate-app\n" +
+        "- generate-component\n" +
+        "- add-component\n" +
+        "---------------------------------------------------------\n"
+    );
   }
 }
 
--- tasks/installation.js
+++ tasks/installation.js
@@ -2,63 +2,88 @@
 
 const readline = require("readline");
 const xsh = require("xsh");
+const logger = require("../lib/logger");
+const chalk = require("chalk");
+const errorHandler = require("../lib/error-handler");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
+  terminal: false
+});
+
+const installXClapCLI = function() {
+  rl.question("Proceed? (y/n)", answer => {
+    if (answer.toLowerCase() === "y") {
+      xsh
+        .exec("npm install -g xclap-cli")
+        .then(function() {
+          logger.log(
+            chalk.green("You've successfully installed the latest xclap-cli.")
+          );
+          rl.close();
+        })
+        .catch(err =>
+          errorHandler(err, "Failed at: Installing the latest xclap-cli.")
+        );
+    }
+  });
+};
+
+const checkXClapCLI = new Promise((resolve, reject) => {
+  xsh
+    .exec(true, "npm list -g --depth=0 | grep xclap-cli")
+    .then(function(version) {
+      const start = version.stdout.indexOf("xclap-cli") + 10;
+      const end = version.stdout.indexOf("\n");
+      resolve(version.stdout.substr(start, end).slice(0, -1));
+    })
+    .catch(function() {
+      resolve();
+    });
+});
+
+const checkXClapCLILatestVersion = new Promise((resolve, reject) => {
+  xsh
+    .exec(true, "npm show xclap-cli version")
+    .then(function(version) {
+      resolve(version.stdout.slice(0, -1));
+    })
+    .catch(err =>
+      errorHandler(err, "Failed at showing the latest xclap-cli version.")
+    );
 });
 
 const Installation = function() {
-  xsh
-    .exec(true, "npm show xclap-cli version")
-    .then(function(npmRegistryVerion) {
-      const xclapLatestVersion = npmRegistryVerion.stdout.slice(0, -1);
-      xsh
-        .exec(true, "npm list -g --depth=0 | grep xclap-cli")
-        .then(function(localVersion) {
-          const start = localVersion.stdout.indexOf("xclap-cli") + 10;
-          const end = localVersion.stdout.indexOf("\n");
-          localVersion = localVersion.stdout.substr(start, end).slice(0, -1);
-
-          if (localVersion === xclapLatestVersion) {
-            console.log(
+  checkXClapCLI.then(function(version) {
+    if (!version) {
+      /* Case 1: xclap-cli does not installed globally */
+      console.log(
+        `Electrode Ignite is about to install the following modules globally:\n- xclap-cli\n`
+      );
+      installXClapCLI();
+    } else {
+      checkXClapCLILatestVersion.then(function(latestversion) {
+        /* Case 2: xclap-cli already got the latest version */
+        if (version === latestversion) {
+          logger.log(
+            chalk.green(
               "Congratulations, you've already installed the latest xclap-cli"
-            );
-          } else if (localVersion < xclapLatestVersion) {
-            console.log(
-              `Electrode Ignite is about to update the following modules globally:\n- xclap-cli (from version ${localVersion} to version ${xclapLatestVersion})`
-            );
-
-            rl.question("Proceed? (Y/n)\n", answer => {
-              if (answer.toLowerCase() === "y") {
-                xsh.exec("npm install -g xclap-cli");
-              }
-            });
-          } else {
-            console.log(
-              "Error when fetching Electrode packages. Exiting now ..."
-            );
-            return process.exit(1);
-          }
-        })
-        .catch(function(err) {
-          // xclap-cli is not get installed
-          console.log(
-            "Electrode Ignite is about to install the following modules globally:\n- xclap-cli\n"
+            )
           );
-
-          rl.question("Proceed? (Y/n)\n", answer => {
-            if (answer.toLowerCase() === "y") {
-              xsh.exec("npm install -g xclap-cli").catch(function(err) {
-                console.log(
-                  "Error when fetching xclap-cli packages. Exiting now ..."
-                );
-                return process.exit(1);
-              });
-            }
-          });
-        });
-    });
+          rl.close();
+        } else if (version < latestversion) {
+          /* Case 3: xclap-cli version is out-dated */
+          console.log(
+            `Electrode Ignite is about to update the following modules globally:\n- xclap-cli (from version ${version} to version ${latestversion})`
+          );
+          installXClapCLI();
+        } else {
+          errorHandler("Error when fetching Electrode packages");
+        }
+      });
+    }
+  });
 };
 
 module.exports = Installation;
