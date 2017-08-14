c7a3b3ed87ee918a4f2e287be0e34ef350b19b8d
Sheng Di <SDi@walmartlabs.com>
2017-08-14 08:17:09 -0700

    generator support

--- tasks/generator.js
+++ tasks/generator.js
@@ -4,22 +4,25 @@ const checkNode = require("../tasks/check-node");
 const errorHandler = require("../lib/error-handler");
 const xsh = require("xsh");
 const { spawn } = require("child_process");
+const Path = require("path");
 
 const Generator = function(type, generator) {
   checkNode()
     .then(function(nodeCheckPassed) {
       if (nodeCheckPassed) {
-        let yoPath = __dirname + "/../node_modules/.bin/yo";
+        let yoPath = "";
         let generatorPath = "";
         let child = "";
 
         if(process.platform.startsWith("win")) {
+          yoPath = Path.join(__dirname, '..', 'node_modules', '.bin', 'yo.cmd');
           yoPath = yoPath.replace(/\//g, "\\");
           generatorPath = generatorPath.replace(/\//g,"\\");
           child = spawn("cmd", ["/c", `${yoPath} ${generator}`], {
             stdio: "inherit"
           });
         } else {
+          yoPath = Path.join(__dirname, '..', 'node_modules', '.bin', 'yo');
           child = spawn(yoPath, [generator], {
             stdio: "inherit"
           });
